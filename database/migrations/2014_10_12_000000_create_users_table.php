6ad85b95a1b5c827a02fb3efe2b46c97a39fad9c
CJona <jonathan.cameron@student.rocvf.nl>
2021-07-03 15:57:31 +0200

    Standaard waarde van tasks kolom verplaatst

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->text('tasks')->default(json_encode([]));
+            $table->text('tasks')->nullable();
             $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
